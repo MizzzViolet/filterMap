58f59220e6ef5dcdb1bf2fe28dbc5990ffed91c4
Jasmine Talukder <violet.xxx17@yahoo.ie>
2021-02-19 00:05:14 +0000

    bump version to 1.0.1

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,12 @@
-const filterMap = (objArray, objName, valueToCompare, outputObjectValue) => {
+"use strict";
+
+var filterMap = function filterMap(objArray, objName, valueToCompare, outputObjectValue) {
   if (!objArray || !objName || !valueToCompare) throw new TypeError("Some required fields are undefined");
-  return objArray.filter(obj => obj[objName] === valueToCompare).map(obj => obj[outputObjectValue || objName]);
+  return objArray.filter(function (obj) {
+    return obj[objName] === valueToCompare;
+  }).map(function (obj) {
+    return obj[outputObjectValue || objName];
+  });
 };
 
 module.exports = filterMap;
